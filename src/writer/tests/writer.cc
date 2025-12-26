c58de8c49ff88de0af86c90892e40c3d30443cde
Korobkov Roman <korobkov.roman1@huawei.com>
2025-12-26 17:28:00 +0300

    Added test for SyncWriter

--- src/writer/tests/writer.cc
+++ src/writer/tests/writer.cc
@@ -6,6 +6,21 @@
 #include <sstream>
 #include <filesystem>
 
+std::string genRandomString(const int len) {
+    static const char alphanum[] =
+        "0123456789"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz";
+    std::string tmp_s;
+    tmp_s.reserve(len);
+
+    for (int i = 0; i < len; ++i) {
+        tmp_s += alphanum[rand() % (sizeof(alphanum) - 1)];
+    }
+    
+    return tmp_s;
+}
+
 TEST(SyncWriter, WriteFixed)
 {
     std::string log = "./sync_writer.log";
@@ -27,6 +42,32 @@ TEST(SyncWriter, WriteFixed)
     
     fileContent << written.rdbuf();
                    
+    EXPECT_EQ(fullLog, fileContent.str());
+
+    std::filesystem::remove(log);
+}
+
+TEST(SyncWriter, WriteRandomString)
+{
+    std::srand((unsigned)time(NULL) * getpid());
+    
+    std::string log = "./sync_writer_" + genRandomString(10) + ".log";
+    
+    std::string fullLog;
+    SyncWriter writer(log);
+
+    for (int i = 0; i < 1000; ++i) {
+        int len = 100 + std::rand() % 50;
+        std::string entry = genRandomString(len);
+        fullLog.append(entry + "\n");
+        
+        writer.write(std::move(entry));
+    }
+
+    std::ifstream written(log);
+    std::stringstream fileContent;
+    fileContent << written.rdbuf();
+
     EXPECT_EQ(fullLog, fileContent.str());
 
     std::filesystem::remove(log);
